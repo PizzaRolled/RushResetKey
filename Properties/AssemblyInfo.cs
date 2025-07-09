a335aa5b5513e2f7af06ed04783fa80793e06836
Pizza Roll <nathan27701024@gmail.com>
2025-07-08 19:49:35 -0500

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ResetRushButton")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ResetRushButton")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9545a306-bec5-4f49-9a32-e12d02d3f8da")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: MelonInfo(typeof(RushResetKey.RushResetKey), "RushResetKey", "1.0.0", "Pizza Roll")]
+[assembly: MelonGame("Little Flag Software, LLC", "Neon White")]
+[assembly: MelonColor(255, 0, 255, 255)]
+[assembly: MelonAuthorColor(255, 225, 237, 0)]
---
+++ RushResetKey.cs
@@ -0,0 +1,60 @@
+﻿using MelonLoader;
+using UnityEngine;
+using UniverseLib.Input;
+
+namespace RushResetKey
+{
+    public class RushResetKey : MelonMod
+    {
+        internal static Game Game { get; private set; }
+        internal static MainMenu MainMenu { get; private set; }
+        internal static bool Resetting { get; private set; } = false;
+
+        public override void OnLateInitializeMelon()
+        {
+            Game = Singleton<Game>.Instance;
+            Game.OnInitializationComplete += OnInitComplete;
+            Settings.Register();
+
+        }
+
+        void OnInitComplete()
+        {
+            MainMenu = MainMenu.Instance();
+        }
+
+        public static void SigmaReset()
+        {
+            if (LevelRush.IsLevelRush() && !RushResetKey.Resetting && Settings.Enabled.Value)
+            {
+                MainMenu.PauseGameNoStateChange(false);
+                Singleton<Audio>.Instance.StopMusic(0f);
+                LevelRush.ClearLevelRushStats();
+                LevelRush.PlayCurrentLevelRushMission();
+                Resetting = true;
+            }
+        }
+        public override void OnUpdate()
+        {
+            if (InputManager.GetKeyDown(Settings.ResetKey.Value))
+                SigmaReset();
+            if (InputManager.GetKeyUp(UnityEngine.KeyCode.Semicolon))
+                Resetting = false;
+
+        }
+        public static class Settings {
+            public static MelonPreferences_Category Category;
+            public static MelonPreferences_Entry<bool> Enabled;
+            public static MelonPreferences_Entry<KeyCode> ResetKey;
+
+            public static void Register()
+            {
+                Category = MelonPreferences.CreateCategory("RushResetKey");
+                Enabled = Category.CreateEntry("Enabled", true);
+                ResetKey = Category.CreateEntry("Reset Key", KeyCode.Semicolon, description: "Pressing the assigned key will reset the current level rush.");
+            }
+
+        }
+    }
+}
+
